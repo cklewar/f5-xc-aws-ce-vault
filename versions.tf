9e0fdaf65a1e47bd08f82f6eb5a160cd8034c555
cklewar <cklewar@t-online.de>
2023-05-11 14:11:10 +0200

    0.11.21 v2

---
+++ versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.3.0"
+  cloud {
+    organization = "cklewar"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "f5-xc-aws-ce-vault-module"
+    }
+  }
+
+  required_providers {
+    volterra = {
+      source  = "volterraedge/volterra"
+      version = "= 0.11.21"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.51.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = ">= 3.15.2"
+    }
+
+    local = ">= 2.2.3"
+    null  = ">= 3.1.1"
+  }
+}
