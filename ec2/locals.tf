6299f31170e110ba73e478a424f623316777916a
cklewar <cklewar@t-online.de>
2023-11-09 15:23:39 +0100

    0.11.23 v17

--- ec2/locals.tf
+++ ec2/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  template_input_dir_path  = var.template_input_dir_path != "" ? var.template_input_dir_path : abspath("../../templates/")
+  template_input_dir_path  = var.template_input_dir_path != "" ? var.template_input_dir_path : abspath("../templates/")
   template_output_dir_path = var.template_output_dir_path != "" ? var.template_output_dir_path : abspath("../_out/")
   custom_tags              = merge(var.custom_tags, {
     Owner = var.owner_tag
